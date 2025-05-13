21bec55fb7f43e83b0115af1d25bb5a63022dc8d
Mateus Henrique de Araújo Santos <matthew210605@gmail.com>
2025-05-13 11:01:57 -0300

    Update-turma.dto de fato

--- src/entities/turmas/dto/update-turma.dto.ts
+++ src/entities/turmas/dto/update-turma.dto.ts
@@ -1,4 +1,29 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTurmaDto } from './create-turma.dto';
+import { IsNotEmpty, IsString, Length, IsEnum, Matches } from 'class-validator';
 
-export class UpdateTurmaDto extends PartialType(CreateTurmaDto) {}
+export class UpdateTurmaDto {
+    @IsNotEmpty({ message: 'O código da disciplina é obrigatório.' }) 
+        @IsString({ message: 'O código da disciplina deve ser uma string.' }) 
+        @Length(3,50) 
+        codigoDisciplina: string;
+    
+        @IsNotEmpty({ message: 'O turno é obrigatório.' })
+        @IsString({ message: 'O turno deve ser uma string.' }) 
+        @IsEnum(['Manhã', 'Tarde', 'Noite'], { message: 'O turno deve ser Manhã, Tarde ou Noite.', }) 
+        turno: 'Manhã' | 'Tarde' | 'Noite';
+    
+        @IsNotEmpty({ message: 'O código da turma é obrigatório.' })
+        @IsString({ message: 'O código da turma deve ser uma string.' })
+        @Matches(/^[A-Za=z0-9_-]+$/, { message: 'O código da turma deve conter apenas letras e números, hífen ou underscore.' })
+         codigoTurma: string;
+    
+        @IsNotEmpty({ message: 'O nome da turma é obrigatório.' }) 
+        @IsString({ message: 'O nome da turma deve ser uma string.' }) 
+        @Length(3,100)
+        nomeTurma: string;
+    
+        @IsNotEmpty({ message: 'O tipo é obrigatório.' })
+        @IsEnum(['aluno', 'professor'], { message: 'O tipo deve ser aluno ou professor.', }) 
+        tipo: 'aluno' | 'professor';
+}
