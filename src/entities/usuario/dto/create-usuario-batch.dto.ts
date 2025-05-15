4cbe52f02fad7e266ef5dce9b0987c840e322d8b
Mateus Henrique de Araújo Santos <matthew210605@gmail.com>
2025-05-15 19:28:10 -0300

    Create usuario batch dto

---
+++ src/entities/usuario/dto/create-usuario-batch.dto.ts
@@ -0,0 +1,12 @@
+import { ArrayMinSize, IsArray, IsNotEmpty, IsUUID, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { CreateUsuarioDto } from './create-usuario.dto';
+
+export class CreateUsuarioBatchDto {
+
+@IsArray()
+@ArrayMinSize(1, { message: 'Deve haver pelo menos uma turma.' })
+@ValidateNested({ each: true })
+@Type(() => CreateUsuarioDto)
+turmas: CreateUsuarioDto[];
+}
