33b1324c51788e3b06bb314658cf101a8487859a
Mateus Henrique de Araújo Santos <matthew210605@gmail.com>
2025-05-20 17:08:52 -0300

    Incrementação da validação em dto turmas

--- src/entities/turmas/dto/create-turma.dto.ts
+++ src/entities/turmas/dto/create-turma.dto.ts
@@ -1,3 +1,4 @@
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString, Matches, IsMongoId, IsArray, IsOptional, IsEnum, Length } from 'class-validator';
 
 
@@ -6,7 +7,9 @@ export class CreateTurmaDto {
 
     @IsNotEmpty({ message: 'O código da disciplina é obrigatório.' }) 
     @IsString({ message: 'O código da disciplina deve ser uma string.' }) 
+    @Transform(({ value }) => value?.trim())
     @Length(3,50) 
+    @Matches(/^[A-Za-z0-9_-]+$/, { message: 'O código da disciplina deve conter apenas letras e números, hífen ou underscore.' })
     codigoDisciplina: string;
 
     @IsNotEmpty({ message: 'O turno é obrigatório.' })
@@ -16,16 +19,21 @@ export class CreateTurmaDto {
 
     @IsNotEmpty({ message: 'O código da turma é obrigatório.' })
     @IsString({ message: 'O código da turma deve ser uma string.' })
+    @Transform(({ value }) => value?.trim())
+    @Length(2,20, { message: 'O código da turma deve ter entre 2 e 20 caracteres.' })	
     @Matches(/^[A-Za=z0-9_-]+$/, { message: 'O código da turma deve conter apenas letras e números, hífen ou underscore.' })
      codigoTurma: string;
 
     @IsNotEmpty({ message: 'O nome da turma é obrigatório.' }) 
     @IsString({ message: 'O nome da turma deve ser uma string.' }) 
+    @Transform(({ value }) => value?.trim())
+    @Matches(/^[A-Za-zÀ-ÿ0-9\s-_.()]+$/, {message: 'O nome da turma contém caracteres inválidos.',})
     @Length(3,100)
     nomeTurma: string;
 
     @IsNotEmpty({ message: 'O tipo é obrigatório.' })
     @IsEnum(['aluno', 'professor'], { message: 'O tipo deve ser aluno ou professor.', }) 
+    @IsString({ message: 'O tipo deve ser uma string.' })
     tipo: 'aluno' | 'professor';
 
     @IsOptional()
