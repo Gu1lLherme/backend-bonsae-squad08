c147dcedb9b604b4e9dfe5c0f00f6c5f0953c64a
Mateus Henrique de Araújo Santos <matthew210605@gmail.com>
2025-05-25 15:28:36 -0300

    Update teste em batch dto

--- src/entities/turmas/dto/create-turma-batch.dto.ts
+++ src/entities/turmas/dto/create-turma-batch.dto.ts
@@ -1,8 +1,13 @@
-import { ArrayMinSize, IsArray, IsNotEmpty, IsUUID, ValidateNested } from 'class-validator';
+import { ArrayMinSize, IsArray, IsNotEmpty, IsOptional, IsUUID, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 import { CreateTurmaDto } from './create-turma.dto';
 
 export class CreateTurmaBatchDto {
+
+@IsOptional()
+@IsUUID('4', { message: 'ID do lote deve ser um UUID válido.' })
+processId?: string;
+
 
 @IsArray()
 @ArrayMinSize(1, { message: 'Deve haver pelo menos uma turma.' })
