4f9af8736e1b2fa80ac8dd2e4d707f7a0af99e0d
Mateus Henrique de Araújo Santos <matthew210605@gmail.com>
2025-05-25 16:07:08 -0300

    Update no dto e controller de periodo-letivo para teste de processo

---
+++ src/entities/periodo-letivo/dto/import-periodo-letivo.dto.ts
@@ -0,0 +1,17 @@
+import { Type } from 'class-transformer';
+import {
+  IsUUID,
+  IsNotEmpty,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
+import { CreatePeriodoLetivoDto } from './create-periodo-letivo.dto';
+
+export class ImportPeriodoLetivoDto {
+  @IsUUID('4', { message: 'processId deve ser um UUID v4 válido.' })
+  processId: string;
+
+  @ValidateNested()
+  @Type(() => CreatePeriodoLetivoDto)
+  periodo: CreatePeriodoLetivoDto;
+}
