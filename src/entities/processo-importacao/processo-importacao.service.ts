8c5386daeefa4011ffb6d3b71d174eda6f0d3053
Mateus Henrique de Araújo Santos <matthew210605@gmail.com>
2025-05-25 15:09:45 -0300

    Schema para id de processos
    Primera tentativa

---
+++ src/entities/processo-importacao/processo-importacao.service.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+export type ProcessoImportacaoDocument = ProcessoImportacao & Document;
+
+@Schema({timestamps: true})
+export class ProcessoImportacao {
+    @Prop ({ required: true })
+    tipo: 'Periodo-Letivo' | 'Disciplina' | 'Turmas'  | 'Usuario';
+    
+    @Prop ({ required: true })
+    status: 'criado' | 'arquivo-enviado' | 'validando' | 'concluido';
+
+    @Prop ({ required: true })
+    usuario: string;
+
+    @Prop ()
+    nomeArquivo?: string;
+
+    @Prop()
+    totalRegistros?: number;
+
+}
+
+export const ProcessoImportacaoSchema = SchemaFactory.createForClass(ProcessoImportacao);
