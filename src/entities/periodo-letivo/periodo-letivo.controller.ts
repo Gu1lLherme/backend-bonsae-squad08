e850f33d5f7eef754568b48a5abf5e60217272ba
Mateus Henrique de Araújo Santos <matthew210605@gmail.com>
2025-06-03 14:06:48 -0300

    Manutenção de métodos necessários

--- src/entities/periodo-letivo/periodo-letivo.controller.ts
+++ src/entities/periodo-letivo/periodo-letivo.controller.ts
@@ -8,23 +8,7 @@ import { CreatePeriodoLetivoBatchDto } from './dto/create-periodo-letivo-batch.d
 export class PeriodoLetivoController {
   constructor(private readonly periodoLetivoService: PeriodoLetivoService) {}
 
-  @Post()
-  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
-  async create(@Body() createPeriodoLetivoDto: CreatePeriodoLetivoDto) {
-    const periodo = await this.periodoLetivoService.create(createPeriodoLetivoDto);
-    return { message: 'Período letivo criado com sucesso!', data: periodo };
-  }
-
-
-  @Post('bulk')
-    @UsePipes(new ValidationPipe({  whitelist: true, forbidNonWhitelisted: true }))
-    async bulkCreate(@Body() createPeriodoLetivoDto: CreatePeriodoLetivoDto[]) {
-      const periodosCriados = await this.periodoLetivoService.bulkCreate(createPeriodoLetivoDto);
-      return {
-        message: 'Períodos registrados com sucesso!',
-        data: periodosCriados,
-      };
-    }
+  
 
   @Post('Batch')
     async createBatch(@Body() dto: CreatePeriodoLetivoBatchDto) {
@@ -38,28 +22,4 @@ export class PeriodoLetivoController {
       return this.periodoLetivoService.updateInvalidPeriodos(id,updateDto);
     }
 
-  @Get()
-  async findAll() {
-    const periodos = await this.periodoLetivoService.findAll();
-    return { message: 'Lista de períodos letivos!', data: periodos };
   }
-
-  @Get(':id')
-  async findOne(@Param('id') id: string) {
-    const periodo = await this.periodoLetivoService.findOne(id);
-    return { message: 'Detalhes do período letivo!', data: periodo };
-  }
-
-  @Patch(':id')
-  @UsePipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
-  async update(@Param('id') id: string, @Body() updatePeriodoLetivoDto: UpdatePeriodoLetivoDto) {
-    const periodo = await this.periodoLetivoService.update(id, updatePeriodoLetivoDto);
-    return { message: 'Período letivo atualizado com sucesso!', data: periodo };
-  }
-
-  @Delete(':id')
-  async remove(@Param('id') id: string) {
-    await this.periodoLetivoService.remove(id);
-    return { message: 'Período letivo removido com sucesso!' };
-  }
-}
